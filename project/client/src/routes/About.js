046bd9821cf514738905f27488c5979bcc32e2d9
Mi Yon Kim <miyon@pdx.edu>
2020-08-01 03:14:31 -0700

    Create /videos request to server

--- project/client/src/routes/About.js
+++ project/client/src/routes/About.js
@@ -1,13 +1,91 @@
 import React from "react";
+import {Col, Row, Container} from 'react-bootstrap';
 import "./About.css";
+import AboutText from "../components/AboutText";
+import axios from 'axios';
+import Video from '../components/Video';
 
 class About extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      videos: '',
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      videos: '',
+    });
+    this.getVideos()
+      .then(res => this.setState({videos: res}))
+      .catch(err => console.log(err));
+  }
+
+  componentDidMount() {
+    axios.get('/videos')
+      .then(response => {
+        this.setState({
+          videos: response.data.items,
+          isLoading: false
+        })
+      })
+      .then(response => {
+        console.log(this.state.videos);
+      })
+    .catch((error) => {
+        console.log(error);
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h2> About</h2>
-      </div>
+      <section className="container">
+        {this.state.isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <Container>
+            <Row>
+              <Col> 
+                <AboutText />
+              </Col>
+              <Col xs lg="3">
+                <Container>
+                  <Row>
+                    {this.state.videos.map(video => (
+                      <Video
+                        key={video.id.videoId}
+                        id={video.id.videoId}
+                        title={video.snippet.title}
+                        thumbnail={video.snippet.thumbnails.medium.url}
+                      />
+                    ))}
+                  </Row>
+                </Container>   
+              </Col>
+            </Row>
+          </Container>
+          //   <div class="col-6">
+          //     <AboutText />
+          //   </div>
+          //   <div className="col-6 videos">
+          //   {this.state.videos.map(video => (
+          //     <Video
+          //       key={video.id.videoId}
+          //       id={video.id.videoId}
+          //       title={video.snippet.title}
+          //       thumbnail={video.snippet.thumbnails.default.url}
+          //     />
+          //   ))}
+          //   </div>
+          // </div>
+          
+        )}
+      </section>
     );
   }
 }
