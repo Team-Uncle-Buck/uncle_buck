f6f5af36ae71f9bafe5ea5902048c95ab45a3e22
Mi Yon Kim <miyon@pdx.edu>
2020-08-01 03:11:51 -0700

    Request Youtube API to get video from server

--- project/server.js
+++ project/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const port = process.env.PORT || 5000;
+const axios = require('axios');
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || "AIzaSyDJTlvNCx6wd0jd2geQbtHYcbaYhk2Nj10";
 
 require('dotenv').config();
 
@@ -10,8 +12,18 @@ const app =express();
 app.use(morgan('tiny'));
 app.use(cors());
 
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=FinanceIndependence&type=video&key=${GOOGLE_API_KEY}`;
+
 app.get('/videos', (req,res) => {
-  res.json([]);
+  console.log("in the video get request")
+  axios.get(url)
+    .then( (response) => {
+      res.send(response.data);
+      console.log(response.data);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
 });
 
 
