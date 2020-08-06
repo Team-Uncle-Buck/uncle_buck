1f184e48e54ad91e7eb447d7b949e2021714d344
Tom <40807906+tlan2@users.noreply.github.com>
2020-08-05 17:19:14 -0700

    Added logo and favicon. Began form.

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Home from "./routes/Home";
 import About from "./routes/About";
+// import Learn from "./routes/Learn";
 import NoMatch from "./routes/NoMatch";
 import Navigation from "./components/Navigation";
 import Jumbotron from "./components/Jumbotron";
@@ -20,6 +21,7 @@ class App extends Component {
             <Switch>
               <Route path="/" exact={true} component={Home} />
               <Route path="/about" component={About} />
+              {/* <Route path="/about" component={Learn} /> */}
               <Route component={NoMatch} />
             </Switch>
           </Router>
--- project/client/src/routes/Home.js
+++ project/client/src/routes/Home.js
@@ -14,4 +14,55 @@ class Home extends React.Component {
   }
 }
 
+// class CalculateDate extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isGoing: true,
+//       numberOfGuests: 2
+//     };
+
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//   }
+
+//   handleInputChange(event) {
+//     const target = event.target;
+//     const value = target.type === 'checkbox' ? target.checked : target.value;
+//     const name = target.name;
+
+//     this.setState({
+//       [name]: value
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <form>
+//         <label>
+//           Is going:
+//           <input
+//             name="isGoing"
+//             type="checkbox"
+//             checked={this.state.isGoing}
+//             onChange={this.handleInputChange} />
+//         </label>
+//         <br />
+//         <label>
+//           Number of guests:
+//           <input
+//             name="numberOfGuests"
+//             type="number"
+//             value={this.state.numberOfGuests}
+//             onChange={this.handleInputChange} />
+//         </label>
+//       </form>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <Reservation />,
+//   document.getElementById('root')
+// );
+
 export default Home;
