e85458a982880d49cb03872deab61d8a9d29400a
Chad Tolleson <tolleson@pdx.edu>
2020-08-12 13:54:10 -0700

    started adding resources the about page for the website, including links to outside resources, the github repo, and git profiles for each team member

--- project/client/src/components/AboutText.js
+++ project/client/src/components/AboutText.js
@@ -4,27 +4,58 @@ function AboutText() {
   return (
     <div className="about__text">
       <section>
-        <h4>What is Lorem Ipsum?</h4>
+        <h4>About</h4>
         <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-        took a galley of type and scrambled it to make a type specimen book. It has survived not 
-        only five centuries, but also the leap into electronic typesetting, remaining essentially 
-        unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum.
+        This is a Financial Independence Calculator Full-Stack Web App created
+        for the PSU Open Source class in the Summer 2020 by team "Uncle Buck".
+        The calculator is a toy meant for entertainment purposes by allowing
+        the user play around with some number to see if and when they can reach
+        financial independence.
         </p>
       </section>
       <section>
-        <h4>What is Lorem Ipsum?</h4>
+        <h4>Financial Independence</h4>
         <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
-        been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-        took a galley of type and scrambled it to make a type specimen book. It has survived not 
-        only five centuries, but also the leap into electronic typesetting, remaining essentially 
-        unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum.
+        Financial Independence is the status of having enough income (from 
+        investments, passive businesses, real estate, etc.) to pay for one's
+        reasonable living expenses for the rest of one's life without having to
+        rely on formal employment. The core path to achieve financial 
+        independence focuses on maximizing one's savings rate through lower 
+        spending and/or higher income.
+        </p>
+      </section>
+      <section>
+        <h4>!!!  Disclaimer  !!!</h4>
+        <p>
+        This tool is meant for entertainment purposes only. We are not 
+        financial professionals. We assume no risk or responsibility for your 
+        financial affairs, well being, nor do we assume any liability for your
+        decisions. Everybody's financial situation is different, you cannot 
+        rely on a single tool for guidance. For real advice on personal 
+        finance, find a trusted certified public accountant (CPA) or certified
+        financial planner (CFP).
+        </p>
+      </section>
+      <section>
+        <h4>Code of Conduct</h4>
+        <p>
+        LINK TO CODE OF CONDUCT DOCUMENT HERE
+        </p>
+      </section>
+      <section>
+        <h4>License</h4>
+        <p>
+        LINK TO LICENSE HERE
+        </p>
+      </section>
+      <section>
+        <h4>Team Uncle Buck</h4>
+        <p>
+        - [Mi Yon Kim](https://github.com/youn0125)
+        - [Tom More "$ Less Problems" Lancaster](https://github.com/tlan2)
+        - [Pat Radamacher](https://github.com/PatRademacher)
+        - [Chad Tolleson](https://github.com/tollesonpdx)
+        - [Uncle Buck](https://github.com/Team-Uncle-Buck)
         </p>
       </section>
       <section>
