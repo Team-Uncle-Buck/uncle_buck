c42d11455c7f02a8100588f52e55220b40861a4e
Tom <40807906+tlan2@users.noreply.github.com>
2020-08-13 11:09:15 -0700

    Working extended form

--- project/client/src/routes/Home.js
+++ project/client/src/routes/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Home.css";
-import {
-  Container, Col, Form,
-  FormGroup, Label, Input,
-  Button,
-} from 'reactstrap';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import FormGroup from 'react-bootstrap/FormGroup'
+import Label from 'react-bootstrap/FormLabel'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import FormCheck from 'react-bootstrap/FormCheck'
+
 
 class Home extends React.Component {
   constructor() {
@@ -12,37 +17,63 @@ class Home extends React.Component {
   }
   render() {
     return (
-      // <div>
-      //   <h2>Calculate Your FI Date:</h2>
-      // </div>
-        <Container className="App">
-        <h2>Sign In</h2>
-        <Form className="form">
-          <Col>
-            <FormGroup>
-              <Label>Email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="exampleEmail"
-                placeholder="myemail@email.com"
-              />
+          <Form>
+          <FormGroup as={Row} controlId="formHorizontalEmail">
+            <Label column sm={2}>
+              Email
+            </Label>
+            <Col sm={10}>
+              <Form.Control type="email" placeholder="Email" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup as={Row} controlId="formHorizontalPassword">
+            <Label column sm={2}>
+              Password
+            </Label>
+            <Col sm={10}>
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+          <fieldset>
+            <FormGroup as={Row}>
+              <Label as="legend" column sm={2}>
+                Radios
+              </Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="radio"
+                  label="first radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios1"
+                />
+                <Form.Check
+                  type="radio"
+                  label="second radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios2"
+                />
+                <Form.Check
+                  type="radio"
+                  label="third radio"
+                  name="formHorizontalRadios"
+                  id="formHorizontalRadios3"
+                />
+              </Col>
             </FormGroup>
-          </Col>
-          <Col>
-            <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input
-                type="password"
-                name="password"
-                id="examplePassword"
-                placeholder="********"
-              />
-            </FormGroup>
-          </Col>
-          <Button>Submit</Button>
+          </fieldset>
+          <FormGroup as={Row} controlId="formHorizontalCheck">
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Form.Check label="Remember me" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup as={Row}>
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit">Sign in</Button>
+            </Col>
+          </FormGroup>
         </Form>
-      </Container>
     );  
   }
 }
