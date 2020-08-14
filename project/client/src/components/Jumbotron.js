586d2532ef3c53a3f90630fa8bb5d5edddb2bec7
Mi Yon Kim <miyon@pdx.edu>
2020-08-14 12:54:59 -0700

    Change jumbotron layout

--- project/client/src/components/Jumbotron.js
+++ project/client/src/components/Jumbotron.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Jumbotron as Jumbo } from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import './Jumbotron.css';
 
 //Class for the top Jumbotron
@@ -8,16 +9,18 @@ class Jumbotron extends Component {
   render() {
     return (
       <Jumbo>
-        <h1 className="jumbo__title">Tia: "What is financial independence?"</h1>
-        <p className="jumbo__text">Uncle Buck: "Basically it means you can live your life without 
-        having to worry about steady employment to cover your expenses. 
-        You know, off the rat wheel and onto the cheese wheel. You catch 
-        my drift? So you hit the financial independence benchmark and you
-         can do a few things: (a) keep on doing what your doing, just add
-         more to the nest egg so you live even larger later, (b) retire, 
-         if you're personally ready for it, or (c) maybe keep on working 
-         but do it your own way - you know, something you love, maybe 
-         fewer hours, whatever...     What's his last name? Spray?"</p>
+        <Container>
+          <h1 className="jumbo__title">Tia: "What is financial independence?"</h1>
+          <p className="jumbo__text">Uncle Buck: "Basically it means you can live your life without 
+          having to worry about steady employment to cover your expenses. 
+          You know, off the rat wheel and onto the cheese wheel. You catch 
+          my drift? So you hit the financial independence benchmark and you
+          can do a few things: (a) keep on doing what your doing, just add
+          more to the nest egg so you live even larger later, (b) retire, 
+          if you're personally ready for it, or (c) maybe keep on working 
+          but do it your own way - you know, something you love, maybe 
+          fewer hours, whatever...     What's his last name? Spray?"</p>
+         </Container>
       </Jumbo>
     );
   }
