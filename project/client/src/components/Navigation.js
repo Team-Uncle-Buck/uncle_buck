925200a82fb05a1088b2c2ab9e30a3b8693e3834
PatRademacher <prad2@pdx.edu>
2020-08-14 22:00:20 -0700

    pushing navbar style changes

--- project/client/src/components/Navigation.js
+++ project/client/src/components/Navigation.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
-import { Nav, Navbar } from 'react-bootstrap';
+import {useState} from "react";
+import { Nav, Navbar, NavDropdown, NavItem } from 'react-bootstrap';
 import './Navigation.css';
 
 
 //Class for the top navbar
 class Navigation extends Component {
-
+  
+    constructor(props){super(props);
+      this.state = { backgroundcolor: "white", color: '#666666' };
+    }
   render() {
     return (
 
-      <Navbar bg="light" expand="lg" variant="light">
+      <Navbar style={{backgroundColor: this.state.backgroundcolor}} bg="light" expand="lg" variant="light">
         <Navbar bg="light" variant="light">
           <Navbar.Brand href="/">
             <img
@@ -27,6 +31,22 @@ class Navigation extends Component {
           <Nav className="ml-auto">
             <Nav.Link href="/">Calculator</Nav.Link>
             <Nav.Link href="/About">About</Nav.Link>
+            <Nav.Link style={{color: this.state.color}} href="/">Calculator</Nav.Link>
+            <Nav.Link style={{color: this.state.color}} href="/About">About</Nav.Link>
+            <Nav.Link style={{color: this.state.color}} href="/Learn">Learn</Nav.Link>
+            <NavDropdown title="Display Theme" id="basic-nav-dropdown" style={{fontcolor: this.state.color}}>
+                <NavDropdown.Item as="button" id="light_display" onClick={() => this.setState({ backgroundcolor: 'white', color:'#666666'})}>Light</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="dark_display" onClick={() => this.setState({ backgroundcolor: 'black', color: 'white'})}>Dark</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="money_display"onClick={() => this.setState({ backgroundcolor: '#85bb65', color: '#065C27' })}>Money</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="gold_display" onClick={() => this.setState({ backgroundcolor: ' rgba(218,165,32, .9)', color: 'rgba(255, 215, 0, 1)' })}>Gold</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="royal_display" onClick={() => this.setState({ backgroundcolor: 'rgba(61,0, 91, .9)', color:'rgba(209, 178, 224, 1)' })}>Royalty</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="silver_display" onClick={() => this.setState({ backgroundcolor: '#9fa4a7', color: '#eeeeef' })}>Silver</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="ruby_display" onClick={() => this.setState({ backgroundcolor: 'rgb(220, 88, 117)', color: 'rgb(103, 26, 26'})}>Ruby</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="blue_display" onClick={() => this.setState({ backgroundcolor: 'rgba(25,68,121,1)', color: 'rgba(152, 193, 220, .8)' })}>Blue Diamond</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="emerald_display" onClick={() => this.setState({ backgroundcolor: 'rgba(23,43,57, 1)', color: 'rgba(26, 177, 136, .7)' })}>Emerald</NavDropdown.Item>
+                <NavDropdown.Item as="button" id="pink_display" onClick={() => this.setState({ backgroundcolor: 'rgba(195,196,196, .7)', color: 'rgba(209, 109, 139, 1)' })}>Perfect Pink</NavDropdown.Item>
+              </NavDropdown>
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
