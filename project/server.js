9a6d9e7245ffaa54dd5fad53da0afd908bb53448
Mi Yon Kim <miyon@pdx.edu>
2020-08-14 19:50:36 -0700

    Change Youtube API Key

--- project/server.js
+++ project/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || "AIzaSyAvEi54OTuwv5lFJ-ac4R61LJ60G1O9qs0";
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || "AIzaSyANrIw_lVdjg6DECyOyxrOl_5rXpD7T9Xc";
 
 const app = express();
 
